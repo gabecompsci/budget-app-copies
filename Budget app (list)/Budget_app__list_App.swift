4bc5c5c68d357bd86cfde4394c2f59cbea169f49
Gabriel Carreno <gabecompsci@gmail.com>
2024-06-26 16:22:41 -0700

    Initial Commit

---
+++ Budget app (list)/Budget_app__list_App.swift
@@ -0,0 +1,17 @@
+//
+//  Budget_app__list_App.swift
+//  Budget app (list)
+//
+//  Created by gabriel carreno on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Budget_app__list_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
